5c46b58b69a370ee9eb8fdf347e4d986a79e768d
codingpratham <coding.job17@gmail.com>
2025-02-22 21:55:07 +0530

    .

--- apps/user-app/app/(dashboard)/companies/page.tsx
+++ apps/user-app/app/(dashboard)/companies/page.tsx
@@ -1,8 +1,28 @@
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+       {comp.map((comp) => (
+              <Card key={comz.id}>
+                <CardHeader>
+                  <CardTitle>{job.title}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-gray-600 mb-2">{job.company}</p>
+                  <p className="text-gray-600 mb-2">{job.location}</p>
+                  <Badge>{job.type}</Badge>
+                </CardContent>
+                <CardFooter className="flex justify-between items-center">
+                  <span className="text-primary font-semibold">{job.salary}</span>
+                  <Button variant="outline">Apply Now</Button>
+                </CardFooter>
+              </Card>
+            ))}
+    </div>
   )
 }
 
